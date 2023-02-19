a16d520180028f2322d68c9357be43f301fbd169
Anna Gallo <heyannagallo@gmail.com>
2023-02-18 23:37:23 -0500

    Rough ingredients list render

--- src/components/Pantry.js
+++ src/components/Pantry.js
@@ -72,22 +72,29 @@ const Pantry = ( { session } ) => {
         'Saving ...'
       ) : (
         // <form onSubmit={updateIngredientsArray} className="form-widget">
-        <form className="form-widget">
-          <div className="container mx-auto w-72 py-4">
-              <input type="text" 
-              name="text" 
-              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" 
-              placeholder="Bananas"
-              id="ingredient"
-              onChange={(e) => setNewIngredient(e.target.value)}
-              />
+        <div>
+            <div>
+                {ingredients.map((ingredient) => {
+                    return <p>{ingredient}</p>;
+                })}
             </div>
-          <div className='text-center'>
-              <button className="w-44 h-11 rounded-full text-gray-50 bg-indigo-600 hover:bg-indigo-700" disabled={loading} onClick={addToIngredients}>
-                Add Ingredient
-              </button>
-          </div>
-        </form>
+            <form className="form-widget">
+              <div className="container mx-auto w-72 py-4">
+                  <input type="text" 
+                  name="text" 
+                  className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" 
+                  placeholder="Bananas"
+                  id="ingredient"
+                  onChange={(e) => setNewIngredient(e.target.value)}
+                  />
+                </div>
+              <div className='text-center'>
+                  <button className="w-44 h-11 rounded-full text-gray-50 bg-indigo-600 hover:bg-indigo-700" disabled={loading} onClick={addToIngredients}>
+                    Add Ingredient
+                  </button>
+              </div>
+            </form>
+        </div>
       )}
       
     </div>
