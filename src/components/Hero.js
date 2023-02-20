5a3ae212b796415a7ab7096dd3df2f5915dd9ebe
Anna Gallo <heyannagallo@gmail.com>
2023-02-20 17:45:19 -0500

    styling and message flow

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,8 @@ import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
-    <div className="container mx-auto flex flex-col items-center justify-center my-24">
-      <h1 className={`${styles.heroText} text-4xl md:text-5xl text-center text-purple-800 font-semibold`}>We have food at home!</h1>
+    <div className="container mx-auto flex flex-col items-center justify-center my-12 md:my-24">
+      <h1 className={`${styles.heroText} text-3xl md:text-5xl text-center text-purple-800 font-semibold`}>We have food at home!</h1>
       <p className=""></p>
     </div>
   );
--- src/components/Pantry.js
+++ src/components/Pantry.js
@@ -5,12 +5,14 @@ import AddIngredient from "./AddIngredient";
 import Ingredient from "./Ingredient";
 
 const Pantry = ({ session }) => {
+  // let selectedIngredients = [];
+
   const [loading, setLoading] = useState(true);
   const [ingredients, setIngredients] = useState(null);
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
   const [recipesReady, setRecipesReady] = useState(false);
   const [recipeData, setRecipeData] = useState(null);
-  const [recipesMessage, setRecipeMessage] = useState('');
-  let selectedIngredients = [];
+  const [recipesMessage, setRecipeMessage] = useState("");
 
   useEffect(() => {
     getIngredients();
@@ -41,11 +43,15 @@ const Pantry = ({ session }) => {
     const { value, checked } = e.target;
 
     if (checked) {
-      selectedIngredients = [...selectedIngredients, value];
+      setSelectedIngredients([...selectedIngredients, value]);
+      // selectedIngredients = [...selectedIngredients, value];
     } else {
-      selectedIngredients = selectedIngredients.filter((e) => e !== value);
+      setSelectedIngredients(selectedIngredients.filter((e) => e !== value));
+      // selectedIngredients = selectedIngredients.filter((e) => e !== value);
     }
-    console.log(selectedIngredients);
+    if (selectedIngredients.length === 0){
+      setRecipesReady(false)
+    }
   };
 
   const updateIngredients = async (e) => {
@@ -74,7 +80,7 @@ const Pantry = ({ session }) => {
   };
 
   const fetchRecipes = async () => {
-    setRecipeMessage('Fetching recipes...')
+    setRecipeMessage("Fetching recipes...");
     fetch(
       `https://api.edamam.com/api/recipes/v2?type=public&q=${selectedIngredients}&app_id=${process.env.REACT_APP_EDAMAM_APP_ID}&app_key=${process.env.REACT_APP_EDAMAM_APP_KEY}`
     )
@@ -83,18 +89,18 @@ const Pantry = ({ session }) => {
         setRecipeData(hits);
         setRecipesReady(true);
         console.log(hits);
-        setRecipeMessage('')
+        setRecipeMessage("");
       })
       .catch(() => {
-        setRecipeMessage('Error loading recipes. Please try again.')
+        setRecipeMessage("Error loading recipes. Please try again.");
         console.error("Could not load recipes");
         setRecipesReady(false);
       });
   };
 
   return (
-    <div className="container flex justify-center">
-      <div aria-live="polite" className="w-full">
+    <div className="container">
+      <div aria-live="polite" className="flex flex-col w-full items-center">
         {loading ? (
           <p className="text-xl text-center mb-8 text-zinc-700 font-semibold">
             Updating pantry...
@@ -111,7 +117,9 @@ const Pantry = ({ session }) => {
               onChange={handleIngredientCheck}
             >
               <legend className="text-xl text-center mb-8 text-zinc-700 font-semibold">
-                Curently in your pantry
+                {ingredients.length} ingredients currently in your pantry.{(selectedIngredients.length === 0 && ingredients.length > 0) && (
+               <p className="text-lg text-zinc-500 font-normal mt-2">Select ingredients to search recipes.</p>
+              )}
               </legend>
               {ingredients.sort().map((ingredient) => {
                 return (
@@ -121,22 +129,24 @@ const Pantry = ({ session }) => {
             </fieldset>
           </div>
         )}
+        {(selectedIngredients.length >= 1) && ( 
         <button
-          className="mx-auto rounded-full px-8 py-4 text-lg font-semibold text-white cursor-pointer bg-purple-700 hover:bg-purple-800"
+          className="mx-auto my-5 rounded-full px-8 py-4 text-lg font-semibold text-white cursor-pointer bg-purple-700 hover:bg-purple-800"
           disabled={loading}
           onClick={fetchRecipes}
         >
           Search Recipes
         </button>
-        {recipesReady ? (
+       )}
+        {(recipesReady && (selectedIngredients.length >= 1)) ? (
           <>
             {recipeData.map((item) => {
-              return <span key={item.recipe.label}>{item.recipe.label}</span>;
+              return <span key={item.recipe.uri}>{item.recipe.label}</span>;
             })}
           </>
         ) : (
           <p className="text-xl text-center mb-8 text-zinc-700 font-semibold">
-            { recipesMessage }
+            {recipesMessage}
           </p>
         )}
       </div>
