34f13e8e2dadb8fac3d60907913ded3288ade10f
Anna Gallo <61474460+galloanna@users.noreply.github.com>
2023-02-20 01:27:48 -0500

    add auth redirect for production url (#5)

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -10,7 +10,7 @@ export default function Auth() {
 
     try {
       setLoading(true)
-      const { error } = await supabase.auth.signIn({ email })
+      const { error } = await supabase.auth.signIn({ email }, {redirectTo: window.location.origin})
       if (error) throw error
       alert('Check your email for the login link!')
     } catch (error) {
