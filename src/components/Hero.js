003ed09e991c8561959124a3171eebf89bebb110
Anna Gallo <heyannagallo@gmail.com>
2023-02-21 02:18:58 -0500

    pass text to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,9 @@
 import styles from './Hero.module.css';
 
-const Hero = () => {
+const Hero = ({ heroText }) => {
   return (
     <div className="container mx-auto flex flex-col items-center justify-center my-12 md:my-24">
-      <h1 className={`${styles.heroText} text-3xl md:text-5xl text-center text-purple-800 font-semibold`}>We have food at home!</h1>
+      <h1 className={`${styles.heroText} text-3xl md:text-5xl text-center text-purple-800 font-semibold`}>{heroText}</h1>
       <p className=""></p>
     </div>
   );
