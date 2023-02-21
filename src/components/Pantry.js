8f5eb690866a6e2028e62e0bed3fd450b6ba714c
Anna Gallo <heyannagallo@gmail.com>
2023-02-20 21:34:56 -0500

    Deletion working

--- src/components/Pantry.js
+++ src/components/Pantry.js
@@ -4,6 +4,7 @@ import { supabase } from "../supabaseClient";
 import AddIngredient from "./AddIngredient";
 import Ingredient from "./Ingredient";
 import RecipeSearchButton from "./RecipeSearchButton";
+import IngredientDeleteButton from "./IngredientDeleteButton";
 import Recipe from "./Recipe";
 
 const Pantry = ({ session }) => {
@@ -11,10 +12,11 @@ const Pantry = ({ session }) => {
 
   const [loading, setLoading] = useState(true);
   const [ingredients, setIngredients] = useState(null);
-  const [selectedIngredients, setSelectedIngredients] = useState([]);
+  const [showButtons, setShowButtons] = useState(false);
   const [recipesReady, setRecipesReady] = useState(false);
   const [recipeData, setRecipeData] = useState(null);
   const [recipesMessage, setRecipeMessage] = useState("");
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
 
   useEffect(() => {
     getIngredients();
@@ -51,12 +53,10 @@ const Pantry = ({ session }) => {
       setSelectedIngredients(selectedIngredients.filter((e) => e !== value));
       // selectedIngredients = selectedIngredients.filter((e) => e !== value);
     }
-    if (selectedIngredients.length === 0) {
-      setRecipesReady(false);
-    }
+    console.log("Selected ingredients are ", selectedIngredients);
   };
 
-  const updateIngredients = async (e) => {
+  const updateIngredients = async () => {
     try {
       setLoading(true);
       const user = supabase.auth.user();
@@ -90,7 +90,6 @@ const Pantry = ({ session }) => {
       .then(({ hits }) => {
         setRecipeData(hits);
         setRecipesReady(true);
-        console.log(hits);
         setRecipeMessage("");
       })
       .catch(() => {
@@ -98,6 +97,43 @@ const Pantry = ({ session }) => {
         console.error("Could not load recipes");
         setRecipesReady(false);
       });
+  };
+
+  const deleteIngredients = async () => {
+    const allIngredients = ingredients;
+    const ingredientsToDelete = selectedIngredients;
+    const deletionSet = new Set(ingredientsToDelete);
+    const remainingIngredients = allIngredients.filter((ingredient) => {
+      return !deletionSet.has(ingredient);
+    });
+    console.log("Ingredients left after deletion: ", remainingIngredients);
+
+    try {
+      setLoading(true);
+      const user = supabase.auth.user();
+
+      const updates = {
+        id: user.id,
+        ingredients: remainingIngredients,
+        updated_at: new Date(),
+      };
+
+      let { error } = await supabase
+        .from("profiles")
+        .upsert(updates, { returning: "minimal" });
+
+      if (error) {
+        throw error;
+      }
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setSelectedIngredients([]);
+      setRecipesReady(false);
+      setLoading(false);
+    }
+
+    getIngredients();
   };
 
   return (
@@ -121,12 +157,9 @@ const Pantry = ({ session }) => {
               >
                 <legend className="text-xl text-center mb-8 text-zinc-700 font-semibold">
                   {ingredients.length} ingredients currently in your pantry.
-                  {selectedIngredients.length === 0 &&
-                    ingredients.length > 0 && (
-                      <p className="text-lg text-zinc-500 font-normal mt-2">
-                        Select ingredients to search recipes.
-                      </p>
-                    )}
+                  <p className="text-lg text-zinc-500 font-normal mt-2">
+                    Select ingredients to search recipes.
+                  </p>
                 </legend>
                 {ingredients.sort().map((ingredient) => {
                   return (
@@ -137,19 +170,19 @@ const Pantry = ({ session }) => {
             </form>
           </div>
         )}
-        {selectedIngredients.length >= 1 && (
+        <div className="flex gap-2">
           <RecipeSearchButton loading={loading} fetchRecipes={fetchRecipes} />
-        )}
-        {recipesReady && selectedIngredients.length >= 1 ? (
+          <IngredientDeleteButton deleteIngredients={deleteIngredients} />
+        </div>
+        <p className="text-xl text-center mb-8 text-zinc-700 font-semibold">
+          {recipesMessage}
+        </p>
+        {recipesReady && (
           <ul className="flex gap-2 flex-wrap items-center justify-center">
             {recipeData.map((item) => {
               return <Recipe item={item} />;
             })}
           </ul>
-        ) : (
-          <p className="text-xl text-center mb-8 text-zinc-700 font-semibold">
-            {recipesMessage}
-          </p>
         )}
       </div>
     </div>
